46640bc6004be52a5f64623a27ef88a75b1bb448
Blu-Eagle <32535727+Blu-Eagle@users.noreply.github.com>
2018-05-17 19:04:28 +0200

    change in the setEnvironmentalData
    whatever was the idea inside the function was not working, I just simplified it and made it working.
    hope you do not mind

--- Adafruit_CCS811/Adafruit_CCS811.py
+++ Adafruit_CCS811/Adafruit_CCS811.py
@@ -158,18 +158,11 @@ class Adafruit_CCS811(object):
 		not set by the application) to compensate for changes in
 		relative humidity and ambient temperature.'''
 		
-		hum_perc = humidity << 1
-		
-		parts = math.fmod(temperature)
-		fractional = parts[0]
-		temperature = parts[1]
 
-		temp_high = ((temperature + 25) << 9)
-		temp_low = ((fractional / 0.001953125) & 0x1FF)
-		
-		temp_conv = (temp_high | temp_low)
+		hum_perc = (int)(humidity *1024/2)
+		temp_conv = (int)((temperature +25)*1024/2)
 
-		buf = [hum_perc, 0x00,((temp_conv >> 8) & 0xFF), (temp_conv & 0xFF)]
+		buf = [((hum_perc >> 8) & 0xFF), (hum_perc & 0xFF),((temp_conv >> 8) & 0xFF), (temp_conv & 0xFF)]
 		
 		self._device.writeList(CCS811_ENV_DATA, buf)
 
